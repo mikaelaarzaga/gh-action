c48d82bfb1af2402aeceee4a979c6afe2f87b9cc
MIKAELA MAE ENOJO ARZAGA <105556245+mikaelaarzaga@users.noreply.github.com>
2024-09-27 16:02:46 +0800

    Create variables.tf

---
+++ my-terraform-module/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
